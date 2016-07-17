3e224b5c59c951533fd3a4ca65cd55be3d6af060
Lukas Senger <lukas@fridolin.com>
2016-07-17 20:25:36 +0200

    add dimension calculation

--- laserbox.go
+++ laserbox.go
@@ -211,12 +211,18 @@ func do(width, height, depth, material, teeth float64, lid bool) {
 	height = height + 2*material
 	depth = depth + material
 	draw(depth+material, depth+material, width, height, depth, material, teeth)
+	totalHeight := int(height + 2*depth + 2*material)
 	if lid {
 		width = width + 2*material
 		height = height + 2*material
 		depth = depth + material
 		draw(depth+material, depth+2*depth+height, width, height, depth, material, teeth)
+		totalHeight += int(height + 2*depth + 4*material)
 	}
+	totalWidth := int(width + 2*depth + 2*material)
+	svg.Width = fmt.Sprintf("%dmm", totalWidth)
+	svg.Height = fmt.Sprintf("%dmm", totalHeight)
+	svg.ViewBox = fmt.Sprintf("0 0 %d %d", totalWidth, totalHeight)
 }
 
 // Do will generate the box for the given values.
